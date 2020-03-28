a31055d9c5e6505e356cb9aa3f824b574717b99e
thanksalot123 <56212275+thanksalot123@users.noreply.github.com>
2020-03-29 02:07:34 +0530

    Add files via upload

--- form.js
+++ form.js
@@ -84,3 +84,128 @@ function closeAllSelect(elmnt) {
 /*if the user clicks anywhere outside the select box,
 then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
+
+
+console.log("connected!");
+var first = document.querySelector("#first")
+var second = document.getElementById("second")
+var button = document.getElementById("third")
+var fuel = document.getElementById("fourth")
+var winning = 0;
+var temp;
+var co2;
+var four;
+var info = document.querySelector('.panel');
+info.addEventListener('submit', (e) => {
+  console.log("its working")
+    e.preventDefault();
+    const km = info.km.value;
+    const type = info.type.value;
+    const fuel = info.fuel.value;
+    winning = Number(first.value);
+    console.log(winning)
+    temp = (second.value);
+    console.log(temp)
+    four = (fourth.value)
+    if (temp === "KTM Suzuki") {
+
+        if (four === "petrol") {
+            co2 = (winning * 2.31) / 23;
+
+        }
+        else if (four === "diesel") {
+            co2 = (winning * 2.68) / 23;
+        }
+        alert(co2);
+    }
+    if (temp === "Yamaha Bajaj") {
+        if (four === "petrol") {
+            co2 = (winning * 2.31) / 28;
+        }
+        else if (four === "diesel") {
+            co2 = (winning * 2.68) / 28;
+        }
+        alert(co2);
+    }
+    if (temp === "Mahindra Vespa") {
+        if (four === "petrol") {
+            co2 = (winning * 2.31) / 30;
+        }
+        else if (four === "diesel") {
+            co2 = (winning * 2.68) / 30;
+        }
+        alert(co2);
+    }
+    if (temp === "zuki Hayabusa") {
+        if (four === "petrol") {
+            co2 = (winning * 2.31) / 34;
+        }
+        else if (four === "diesel") {
+            co2 = (winning * 2.68) / 34;
+        }
+        alert(co2);
+    }
+    if (temp === "MV Agusta") {
+        if (four === "petrol") {
+            co2 = (winning * 2.31) / 43;
+        }
+        else if (four === "diesel") {
+            co2 = (winning * 2.68) / 43;
+        }
+        alert(co2);
+    }
+    if (temp === "Super Blackbird") {
+        if (four === "petrol") {
+            co2 = (winning * 2.31) / 50;
+        }
+        else if (four === "diesel") {
+            co2 = (winning * 2.68) / 50;
+        }
+        alert(co2);
+    }
+    if (temp === "Agusta") {
+        if (four === "petrol") {
+            co2 = (winning * 2.31) / 60;
+        }
+        else if (four === "diesel") {
+            co2 = (winning * 2.68) / 60;
+        }
+        alert(co2);
+}
+    if (temp === "Royal Enfield Classic") {
+        if (four === "petrol") {
+            co2 = (winning * 2.31) / 65;
+        }
+        else if (four === "diesel") {
+            co2 = (winning * 2.68) / 65;
+        }
+        alert(co2);
+    }
+    if (temp === "Honda Activa ") {
+        if (four === "petrol") {
+            co2 = (winning * 2.31) / 74;
+        }
+        else if (four === "diesel") {
+            co2 = (winning * 2.68) / 74;
+        }
+        alert(co2);
+        }
+    if (temp === "Bajaj Pulsar") {
+        if (four === "petrol") {
+            co2 = (winning * 2.31) / 55;
+        }
+        else if (four === "diesel") {
+            co2 = (winning * 2.68) / 55;
+        }
+        alert(co2);
+    }
+    if (temp === "Ducati") {
+        if (four === "petrol") {
+            co2 = (winning * 2.31) / 44;
+        }
+        else if (four === "diesel") {
+            co2 = (winning * 2.68) / 44;
+        }
+        alert(co2);
+    }
+});
