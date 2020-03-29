490de4c0f1e758fee5c55925ee73cac7da7f2834
thanksalot123 <56212275+thanksalot123@users.noreply.github.com>
2020-03-29 06:55:59 +0530

    Update user.js

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -5,6 +5,8 @@ const status = document.querySelector('#status')
 const lox = document.querySelector('#lock')
 const box = document.querySelector('#bock')
 const hox = document.querySelector('#hock')
+const des = document.querySelector('#description')
+const points = document.querySelector('#points')
 
 
 
@@ -30,6 +32,8 @@ function geoFindMe() {
                 lox.style.background = "linear-gradient(#2ecc71,#16a085)"
                 box.style.background = "linear-gradient(#2ecc71,#16a085)"
                 hox.style.background = "linear-gradient(#2ecc71,#16a085)"
+                des.textContent = "  DESCRIPTION"
+                points.textContent = "you are safe and there is no harm to you due to your locality air quality"
                 
             }else if(data.aqius>50 && data.aqius<=100){
                 status.textContent = 'Moderate'
@@ -38,6 +42,8 @@ function geoFindMe() {
                 lox.style.background = "linear-gradient(#f9ca24,#f6e58d)"
                 box.style.background = "linear-gradient(#f9ca24,#f6e58d)"
                 hox.style.background = "linear-gradient(#f9ca24,#f6e58d)"
+                des.textContent = "  DESCRIPTION"
+                points.textContent = "is acceptable; however, pollution in this range may pose a moderate health concern for a very small number of individuals. People who are unusually sensitive to ozone or particle pollution may experience respiratory symptoms"
 
             }else if(data.aqius>100 && data.aqius<=150){
                 status.textContent = 'Not safe'
@@ -46,6 +52,8 @@ function geoFindMe() {
                 lox.style.background = "linear-gradient(#f1c40f,#f39c12)"
                 box.style.background = "linear-gradient(#f1c40f,#f39c12)"
                 hox.style.background = "linear-gradient(#f1c40f,#f39c12)"
+                des.textContent = "  DESCRIPTION"
+                points.textContent = "if aqi is between 101 and 150, members of sensitive groups may experience health effects, but the general public is unlikely to be affected.Ozone: People with lung disease, children, older adults, and people who are active outdoors are considered sensitive and therefore at greater risk. Particle pollution: People with heart or lung disease, older adults,1 and children are considered sensitive and therefore at greater risk."
 
             }else if(data.aqius>150 && data.aqius<=200){
                 status.textContent = 'UN-HEALTHY!!'
@@ -54,6 +62,8 @@ function geoFindMe() {
                 lox.style.background = "linear-gradient(#c0392b,#e74c3c)"
                 box.style.background = "linear-gradient(#c0392b,#e74c3c)"
                 hox.style.background = "linear-gradient(#c0392b,#e74c3c)"
+                des.textContent = "  DESCRIPTION"
+                points.textContent = "Everyone may begin to experience health effects when AQI values are between 151 and 200. Members of sensitive groups may experience more serious health effects."
 
             }else if(data.aqius>200 && data.aqius<=300){
                 status.textContent = 'DANGEROUS!!'
@@ -62,6 +72,8 @@ function geoFindMe() {
                 lox.style.background = "linear-gradient(#8e44ad,#9b59b6)"
                 box.style.background = "linear-gradient(#8e44ad,#9b59b6)"
                 hox.style.background = "linear-gradient(#8e44ad,#9b59b6)"
+                des.textContent = "  DESCRIPTION"
+                points.textContent = "Very Unhealthy. AQI values between 201 and 300 trigger a health alert, meaning everyone may experience more serious health effects"
 
             }else if(data.aqius>300 && data.aqius<=500){
                 status.textContent = 'HAZARDOUS'
@@ -70,6 +82,9 @@ function geoFindMe() {
                 lox.style.background = "linear-gradient(#6F1E51,#833471)"
                 box.style.background = "linear-gradient(#6F1E51,#833471)"
                 hox.style.background = "linear-gradient(#6F1E51,#833471)"
+                des.textContent = "  DESCRIPTION:"
+                points.textContent = "AQI values over 300 trigger health warnings of emergency conditions. The entire population is even more likely to be affected by serious health effects."
+ 
 
             }
 
